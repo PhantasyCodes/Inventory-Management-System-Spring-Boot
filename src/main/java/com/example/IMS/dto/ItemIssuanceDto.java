c6b24c5946656b7d5b58987678abe7e2247aa01b
David King <davidkingmwai@gmail.com>
2025-04-13 14:04:43 +0300

    Updates

--- src/main/java/com/example/IMS/dto/ItemIssuanceDto.java
+++ src/main/java/com/example/IMS/dto/ItemIssuanceDto.java
@@ -1,8 +1,8 @@
 package com.example.IMS.dto;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public class ItemIssuanceDto {
 	private long id;
